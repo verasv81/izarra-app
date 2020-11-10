4d11bb857fe5735ac4c82ba361dfd9a72efc1513
Vera Șveț <verasv997@gmail.com>
2020-11-10 21:51:49 +0200

    some adjusts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { BlogComponent } from './blog/blog.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import { HomeComponent } from './home/home.component';
-import { CatalogComponent } from './catalog/catalog.component';
-import { BlogPreviewComponent } from './blog-preview/blog-preview.component';
-import { ServiceDetailsComponent } from './service-details/service-details.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/bolg/blog.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CatalogComponent } from './pages/services/catalog/catalog.component';
+import { BlogPreviewComponent } from './pages/bolg/blog-preview/blog-preview.component';
+import { ServiceDetailsComponent } from './pages/services/service-details/service-details.component';
 
 const routes: Routes = [
   { path: 'contacts', component: ContactsComponent },
